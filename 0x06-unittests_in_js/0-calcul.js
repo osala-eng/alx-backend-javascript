965cfb4382e9e6f76bb0f880cb1d9e98eb0e6574
Jashon Osala <jashonosala@gmail.com>
2023-08-24 23:22:00 +0300

    Create 0-calcul.js

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,7 @@
+/**
+ * A function that accepts two numbers and returns the sum of the two numbers.
+ * @param {number} a  Number a
+ * @param {number} b Number b
+ * @returns Sum of a and b
+ */
+module.exports = (a, b) => Math.round(a) + Math.round(b);
